a94bd6ba4661960106755bfca1f75b3a19962a24
Eky Zakariah <ekyzakariah02@gmail.com>
2022-05-25 13:57:36 +0700

    xd_site: Initial Landing Page

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const navMenu = document.getElementById("nav-menu");
+const toggle = document.getElementById("nav-toggle");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+
+const showMenu = (toggleId, navId, mainId) => {
+   if (toggle && navMenu) {
+      toggle.addEventListener("click", () => {
+         navMenu.classList.toggle("show-menu");
+         main.classList.toggle("main-blur");
+      });
+   }
+
+   if (main) {
+      main.addEventListener("click", () => {
+         navMenu.classList.remove("show-menu");
+      });
+   }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((link) =>
+   link.addEventListener("click", () => {
+      navMenu.classList.toggle("show-menu");
+   })
+);
+
+window.onscroll = () => {
+   if (window.scrollY > 1) {
+      navMenu.classList.remove("show-menu");
+   }
+};
